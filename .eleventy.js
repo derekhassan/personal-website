821c8ebcb6f595cacc0aa6f5abcbcdc4e63d5812
Derek H <dhass421@gmail.com>
2022-09-10 20:45:25 -0400

    Added code block syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+const markdownItAttrs = require('markdown-it-attrs');
+
 module.exports = (eleventyConfig) => {
+    const options = {
+        html: true,
+        breaks: true,
+        linkify: true,
+    };
+    const markdownLib = markdownIt(options).use(markdownItAttrs);
+    eleventyConfig.setLibrary('md', markdownLib);
+
+    eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPassthroughCopy('./src/*.css');
     eleventyConfig.addPassthroughCopy('./src/js');
 
